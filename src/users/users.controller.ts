99e374f26570998dc153323239bd41c4129a50dc
Franco Odone <108704414+Franco-Odone@users.noreply.github.com>
2023-05-23 16:48:22 -0400

    getUsers and createUser added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  // Con : Promise<User[]> se indica que este método devuelve una promesa que a su vez devuelve un array con entidades de tipo User
+  // Recordar que estamos en una clase por lo que this. se usa para indicar el uso de parámetros del constructor en el mimso método
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(newUser);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // @InjectRepository sirve para importar entities en un "Repository" para comunicarse con la db
+  // A la derecha del decorador va el nombre del repositorio
+  // A la derecha del nombre el tipo de dato (recordar que se está usando typescript)
+  // entre <> se indica que el repositorio contiene un tipo de dato que en este caso es el usuario que hemos importado (entity)
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  // Recordar que estamos en una clase por lo que this. se usa para indicar el uso de parámetros del constructor en el mimso método
+  createUser(user: CreateUserDto) {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+
+  getUsers() {
+    return this.userRepository.find();
+  }
+}
